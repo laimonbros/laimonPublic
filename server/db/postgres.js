7cc7a5d3172201098c09b6587dc44cb0470f1ffb
Laimon Bros <partners@laimonbros.com>
2025-01-15 19:52:48 +0300

    Update postgres.js

--- server/db/postgres.js
+++ server/db/postgres.js
@@ -1,6 +1,6 @@
 export default {
     connect(Sequelize) {
-        return new Sequelize('postgres://cloud_user:WIrn7KKLtH12.@shubologur.beget.app:5432/default_db', {
+        return new Sequelize('INSERTPOSTGRESHERE', {
             logging: false, /* (msg) => {//false
                 console.log(`[Sequelize Log]: ${msg}`);
             },*/
